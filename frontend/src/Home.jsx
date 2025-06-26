efdd294fb7e10fa767a2b70f664bc4925050d753
MOUNISH-SP <mounishsakthivel8399@gmail.com>
2025-06-26 13:27:42 +0530

    updated

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    axios.get('https://login-authentication-pi.vercel.app//api/auth/me', { withCredentials: true })
+    axios.get('https://login-authentication-pi.vercel.app/api/auth/me', { withCredentials: true })
       .then(r => setU(r.data.user))
       .catch(() => setU(null));
   }, []);
